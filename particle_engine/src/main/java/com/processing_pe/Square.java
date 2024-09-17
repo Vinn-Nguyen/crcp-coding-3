6c3a73aa7bb0629477e4c1802a1cce5c70940b1f
Wren Nguyen <vinnn@smu.edu>
2024-09-16 20:51:26 -0500

    couldn't figure out arrays, but it works as a functional particle engine

--- particle_engine/src/main/java/com/processing_pe/Square.java
+++ particle_engine/src/main/java/com/processing_pe/Square.java
@@ -2,8 +2,7 @@
 Name: Wren Nguyen
 Project Name: Particle Engine
 Date: 9.9.2024
-Description: This class contains the info to draw a bouncing square, and contains
-all the commands its able to do.
+Description: This class contains the info for a single square to bounce. 
 */
 
 package com.processing_pe;
@@ -33,10 +32,12 @@ class Square {
     speedY = main.random(-2, 2);
   }
 
-  public Square(Animate animate) { //connects square to animate
+  public void display(){ //show the square
+    main.rectMode(main.CENTER);
+    main.rect(shapeX, shapeY, sz, sz); //draw square
   }
 
-  void bounce(){ //makes the squares bounce
+  public void bounce(){ //makes the squares bounce
     shapeX = shapeX + speedX; //moves the x position
     shapeY = shapeY + speedY; //moves the y position
 
@@ -61,17 +62,50 @@ class Square {
       shapeY = shapeY + speedY - 1;
     }
   }
-  
-  void click(){ //stuff happens when the mouse is clicked.
+
+  public void click(){ //stuff happens when the mouse is clicked.
     //left click reverses speed
     if (main.mousePressed && (main.mouseButton == main.LEFT)){ 
       speedX = speedX * -1;
       speedY = speedY * -1;
     }
-  }
 
-  public void draw(){ //draw the square
-    main.rect(shapeX, shapeY, sz, sz);
-    bounce();
-  }
+    //right click changes background color
+    if (main.mousePressed && (main.mouseButton == main.RIGHT)){ 
+      main.black = main.white;
+    }
+    }
+  
+  /* public void draw(){ //draw everything
+    background (black); //black bg 
+    
+    //display arrayOne
+    for (Square obj : arrayOne){
+      //if mouse on the right, colors is blue
+      if (mouseX > width/2){
+        fill(blue);
+      } else { //on the left, color is pink
+        fill(pink);
+      }
+      //draws the squares from Square class
+      obj.bounce();
+      obj.click();
+      obj.display();
+    }
+    
+    //display arrayTwo
+    for (Square obj : arrayTwo){
+      //if mouse on the bottom, colors is purple
+      if (mouseY > height/2){
+        fill(purple);
+      } else { //on the top, color is yellow
+        fill(yellow);
+      }
+
+      //draw squares from Square class
+      obj.bounce();
+      obj.click();
+      obj.display();  
+    }
+  }*/
 }
