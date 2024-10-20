7c90352badc46c6835964dd3bc9e91f0b6d0a3e4
Wren Nguyen <vinnn@smu.edu>
2024-10-19 23:52:07 -0500

    final commit

--- lists/src/main/java/com/lists/GroceryLinkedList.java
+++ lists/src/main/java/com/lists/GroceryLinkedList.java
@@ -9,9 +9,9 @@ package com.lists;
 
 public class GroceryLinkedList {
     //VARIABLES
-    public int index = 0;
-    public FoodNode head;
-    public FoodNode next;
+    public int currentIndex = 0; //current index
+    public FoodNode head; //start of list
+    public FoodNode next; //next node of list
 
     public GroceryLinkedList(){
         head = null;
@@ -20,51 +20,56 @@ public class GroceryLinkedList {
     
     //PRINT: Traverse and print all the foods in a list
     public void print(){
-        //if the list empty
+        //if the list empty, return list is empty.
         if(isEmpty())
             System.out.println("List is empty.");
         
         FoodNode current = head; //start at the head
-        while (current != null){ //while not at the end of the list
-            System.out.println(current.foodName()); //print list
-            current = current.next; //go through list
+        while (current != null){ //traverse through the list until the end
+            System.out.println(current.foodName()); //print nodes
+            current = current.next; //go to next node
         }
     }
     
     //INSERT AT INDEX: insert a FoodNode at the indicated index 
-    void insert(FoodNode node, int count){
+    void insert(FoodNode node, int index){
+        //if empty, insert node at head
         if(isEmpty()){
             head = node; //Insert at head
         }
-        //insert at specified point
+        //insert at specified index
         else{
-            FoodNode current = head; //traverse at head
-            for(int i=1; i < count && current != null; i++){ //find the index
-                current = current.nextFood();
-                index = count;
+            FoodNode current = head; //start at the head
+            for(int i=1; i < index && current != null; i++){ //create an index while traversing the list
+                current = current.nextFood(); //go to the next node
+                currentIndex = index; //assign a currentIndex to the index
             }
-            current.setNext(node); //insert node at the end
+            current.setNext(node); //insert the node
         } 
     }
 
     //INSERT AFTER FOOD: insert a foodnode after another specified food WIP
     void insert(FoodNode node, String food){
+        //if empty, insert node at head
         if(isEmpty()){
-            head = node; //Insert at head
+            head = node;
         }
-        //insert at specified point
-        FoodNode current = head; //traverse at head
-        while(current.nextFood() != null){
-            current = current.nextFood();
-            if(current.foodName().equals(food)){
-                FoodNode previous = current.next;
-                current.setNext(node);
-                current = current.next;
-                index++;
-                while(previous.nextFood() != null){
-                    current.setNext(previous);
-                    index++;
-                    previous = previous.nextFood();
+
+        //insert at specified food
+        FoodNode current = head; //start at the head
+        while(current.nextFood() != null){ //traverse the list
+            current = current.nextFood(); //go to the next node
+
+            //This section matches the food with inputted foodname
+            if(current.foodName().equals(food)){ //match foodname and food
+                FoodNode previous = current.next; //previous stores the nodes after the foodName
+                current.setNext(node); //insert new food node
+                current = current.next; //go to the next item
+
+                //This section prints the rest of the list after the new node
+                while(previous.nextFood() != null){ //go through the previous list
+                    current.setNext(previous); //create nodes for the previous list
+                    previous = previous.nextFood(); //go the next node of the previous list
                 }
             }
         } 
@@ -74,12 +79,12 @@ public class GroceryLinkedList {
     void insertAtStart(FoodNode node){
         //if its empty, insert at the head of the list
         if(isEmpty())
-            head = node; //insert at head
+            head = node;
 
         else{
-            FoodNode current = head; 
-            head = node;
-            head.next = current;
+            FoodNode current = head; //start at the head
+            head = node; //create a new node at the head
+            head.next = current; //push the other nodes beneath the new head
         }
         
     }
@@ -88,14 +93,13 @@ public class GroceryLinkedList {
     public void insertAtEnd(FoodNode node){
         //if its empty, insert at the head of the list
         if(isEmpty())
-            head = node; //Insert at head
+            head = node;
         
         else{
-            FoodNode current = head; //traverse to the head
-            while(current.nextFood()!= null){ //while not the last node
-                current = current.nextFood();//go to the next node
+            FoodNode current = head; //start at the head
+            while(current.nextFood()!= null){ //traverse through the list
+                current = current.nextFood();//go to the next node until the end
             }
-            index++;
             current.setNext(node); //insert node at the end
         }
     }
@@ -105,20 +109,23 @@ public class GroceryLinkedList {
         return head == null;
     }
 
-    //FIND: find a specified food and return the index WIP
+    //FIND: find a specified food and return the index
     public int find(String food){
         //if the list is empty return 0
         if(isEmpty()){
             return 0;
         }
 
-        //return food node index
-        FoodNode current = head;
-        while((current.nextFood() != null)) //traverse
-            current = current.nextFood();
-            if(current.foodName().equals(food)){ //finds the food based on the name
-                return index; //return index
+        //return foodnode index
+        FoodNode current = head; //start at the head
+        while((current.nextFood() != null)) //traverse through the list
+            current = current.nextFood(); //go to the next node
+
+            if(current.foodName().equals(food)){ //matches the foodName with the food
+                return currentIndex; //returns the index of the foodNode
             }
+
+            //otherwise, it returns -1
             else {
                 return -1;
             }
@@ -130,29 +137,30 @@ public class GroceryLinkedList {
         if(isEmpty())
             return "List is empty"; //returns empty list
 
-        //remove first node
+        //declare/initalize previous & current
         FoodNode current = head;
         FoodNode previous = null;
 
-        if(current.foodName().equals(food)){
-            head = current.nextFood(); //current is at the head
-            index--;
+        //removing the first node
+        if(current.foodName().equals(food)){ //if the food matches the foodName
+            head = current.nextFood(); //the current moves up to the head, removing the old head
+            currentIndex--; //subtract the current index
             return "Removed" + current.foodName(); //return what was deleted
         }
 
         //remove a named node
-        //while not at the end & the food matches
-        while((current.nextFood() != null) && (!current.foodName().equals(food))){ 
-            previous = current;
-            current = current.nextFood();
+        while((current.nextFood() != null) && (!current.foodName().equals(food))){ //traverse list & match food w/ foodName
+            previous = current; //previous stores the list
+            current = current.nextFood(); //goes to the next node
         }
-        if(current.foodName().equals(food)){ //if the current name matches
-            previous.setNext(current.nextFood()); //replace the current with previous
-            index--;
+        if(current.foodName().equals(food)){ //if the current name matches the food
+            previous.setNext(current.nextFood()); //replace the previous node with the current node
+            currentIndex--; //subtract from index
             return "Removed " + current.foodName(); //return what was deleted
         }
-        //node not found
+        
+        //if the food is not found, returns not found and name
         else
-            return ("No entry for " + food); //returns not found and name
+            return ("No entry for " + food);
     }
 }
--- lists/src/main/java/com/lists/Main.java
+++ lists/src/main/java/com/lists/Main.java
@@ -2,7 +2,7 @@
  * Name: Wren Nguyen
  * Date: Oct 2024
  * Project: LinkedList Introduction
- * Description: Main runs all the test
+ * Description: Main, runs all the test and prints everything to terminal
 */
 
 package com.lists;
@@ -10,45 +10,47 @@ package com.lists;
 public class Main {
     public static void main(String[] args) {
 
-        //TEST 1
-        GroceryLinkedList test = new GroceryLinkedList();
+        //TEST 1: create list and print it
+        GroceryLinkedList test = new GroceryLinkedList(); //create linked list
+        //add nodes to list
         test.insert(new FoodNode("milk"), 0);
         test.insert(new FoodNode("bananas"), 1);
         test.insert(new FoodNode("ice cream"), 2);
         test.insert(new FoodNode("spinach"), 3);
         test.insert(new FoodNode("chicken"), 4);
+        //print
         System.out.println("Test 1");
         test.print(); 
 
-        //TEST 2
-        System.out.println("\n" + "Test 2");
-        System.out.println(test.remove("ice cream"));
-        test.print();
+        //TEST 2: remove ice cream and print list
+        System.out.println("\n" + "Test 2"); 
+        System.out.println(test.remove("ice cream")); //remove ice cream
+        test.print(); //print
 
-        //TEST 3
+        //TEST 3: remove chicken and print list
         System.out.println("\n" + "Test 3");
-        System.out.println(test.remove("chicken"));
-        test.print(); 
+        System.out.println(test.remove("chicken")); //remove chicken
+        test.print(); //print
 
-        //TEST 4
+        //TEST 4: find spinach's index (2) and print list
         System.out.println("\n" + "Test 4");
-        System.out.println(test.find("spinach"));
-        test.print();
+        System.out.println(test.find("spinach")); //find spinach's index
+        test.print(); //print
 
-        //TEST 5
-        test.insertAtEnd(new FoodNode("eggs"));
+        //TEST 5: insert eggs, find egg's index (3) and printlist
+        test.insertAtEnd(new FoodNode("eggs")); //insert eggs at the end
         System.out.println("\n" + "Test 5");
-        System.out.println(test.find("eggs"));
-        test.print();
+        System.out.println(test.find("eggs")); //find eggs's index
+        test.print(); //print
 
-        //TEST 6
+        //TEST 6: add onions at the start and print list
         System.out.println("\n" + "Test 6");
-        test.insertAtStart(new FoodNode("onions"));
-        test.print();
+        test.insertAtStart(new FoodNode("onions")); //insert onions at the start
+        test.print(); //print
 
-        //TEST 7
+        //TEST 7: add strawberries after bananas and print list
         System.out.println("\n" + "Test 7");
-        test.insert(new FoodNode("strawberries"), "bananas");
-        test.print();
+        test.insert(new FoodNode("strawberries"), "bananas"); //search for bananas, add strawberries after
+        test.print(); //print
     }
 }
